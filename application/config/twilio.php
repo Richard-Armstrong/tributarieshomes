0c96ba4a2e9cdccbd554967fdfdb197db4b8e2f7
Richard Armstrong <richard.armstrong@gmail.com>
2020-06-16 13:50:26 -0500

    removing twilo data

--- application/config/twilio.php
+++ application/config/twilio.php
@@ -7,12 +7,12 @@ $config['mode'] = 'sandbox';
 /**
 * Account SID
 **/
-$config['account_sid'] = 'AC10b15200b4ad8de1f35d0c18e3ba7657';
+$config['account_sid'] = 'ACAAA10b15200b4ad8de1f35d0c18e3ba7657';
 
 /**
 * Auth Token
 **/
-$config['auth_token'] = '1164e4fca1a19fc3388627ee3deb86a3';
+$config['auth_token'] = '116AAA4e4fca1a19fc3388627ee3deb86a3';
 
 /**
 * API Version
@@ -22,7 +22,7 @@ $config['api_version'] = '2010-04-01';
 /**
 * Twilio Phone Number
 **/
-$config['number'] = '9105448323';
+$config['number'] = '9100005448323';
 
 /* End of file twilio.php */
 ?>
